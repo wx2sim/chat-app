6cacf7588c0452b5b155f797bca314c6bc5e35ce
Wx2sim <wx2sim@gmail.com>
2025-01-08 09:48:51 +0100

    database connection MONGODB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import authRoutes from './routes/auth.route.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT ;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+  console.log("Server is running on port 5001");
+  connectDB();
 });
